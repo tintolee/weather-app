64869d8544870c44b53798457fb2c309fe921e96
Adelaja Oluwatosin <adelajaolutosin@gmail.com>
2020-08-21 14:25:29 +0100

    Updates

---
+++ precache-manifest.2464b698d0f5cd9742bf2687511b6bd8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32e18f80a834c4bd1b032f013e34744",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f4bf695d83c9db243ed1",
+    "url": "/weather-app/static/css/2.487b6fa3.chunk.css"
+  },
+  {
+    "revision": "3f13bffcae4683860920",
+    "url": "/weather-app/static/css/main.295b1454.chunk.css"
+  },
+  {
+    "revision": "f4bf695d83c9db243ed1",
+    "url": "/weather-app/static/js/2.917238d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.917238d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f13bffcae4683860920",
+    "url": "/weather-app/static/js/main.7cea09d1.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  },
+  {
+    "revision": "b1e194ab02fc548228b141796b964f37",
+    "url": "/weather-app/static/media/images.b1e194ab.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
